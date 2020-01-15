8874a689524af77b465131f5a3b0a6311da3fcb9
kevin <zmy9108@gmail.com>
2020-01-15 11:53:19 -0800

    fix the PPA scraper to catch the exception without breaking the scrapping process.

--- ppa_generation.py
+++ ppa_generation.py
@@ -3,8 +3,11 @@ Scape the People Also Ask from a provided list of queries
 """
 import json
 import argparse
-from gquestions_json_qurl import *
-
+import logging
+#from gquestions_json_qurl import *
+#from gquestions_json import *
+from gquestions_json import initBrowser
+from pprint import pprint
 
 def config_args():
     parser = argparse.ArgumentParser()
@@ -16,10 +19,13 @@ def config_args():
                         help="The language for search engine. This should be kept as English")
     parser.add_argument('--in_dir', type=str, default='search_queries.txt',
                         help="The path for the input file with a list of queries that you want to search for google paa")
-    parser.add_argument('--out_dir', type=str, default='result/ppa_output.json',
+    parser.add_argument('--out_dir', type=str, default='result/ppa_output_full.json',
                         help="The path for the output json file")
+    parser.add_argument('--backup_dir', type=str, default='result/ppa_backup.json',
+                        help="The path for the backup json file for ppa")
     parser.add_argument('--headless', action='store_true',
                         help="The flag to indicate whether you want the browser pop up or not.")
+    parser.add_argument('--extraction_mode', type=str, default="full", help="The mode to extract the information")
 
     args = parser.parse_args()
     return args
@@ -29,7 +35,10 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     args = config_args()
-
+    
+    with open(args.out_dir, 'r') as fp:
+        result = json.load(fp)
+    assert type(result) is dict, "The initialized result should be a dictionary"
     #queries = ["sushi"]
     with open(args.in_dir, 'r') as fp:
         queries = fp.readlines()
@@ -44,30 +53,75 @@ if __name__ == "__main__":
         browser = initBrowser()
 
     query = queries[0]
-    result = {query: None for query in queries}
+    #result = {query: None for query in queries}
+    
+    if args.extraction_mode == "qurl":
+        from gquestions_json_qurl import newSearch_Mingyang, crawlQuestions_Mingyang, flatten_paa_list
+        for query in queries:
+            if not result.get(query, None):
+                try:
+                    result[query] = []
 
-    for query in queries:
-        start_paa, start_paa_url = newSearch_Mingyang(
-            browser, query)
+                    start_paa, start_paa_url = newSearch_Mingyang(
+                        browser, query)
 
-        initialSet = {}
-        cnt = 0
-        for q, url in zip(start_paa, start_paa_url):
-            initialSet.update({cnt: {"q": q, "url": url}})
-            cnt += 1
+                    initialSet = {}
+                    cnt = 0
+                    for q, url in zip(start_paa, start_paa_url):
+                        initialSet.update({cnt: {"q": q, "url": url}})
+                        cnt += 1
 
-        paa_list = []
+                    paa_list = []
 
-        crawlQuestions_Mingyang(start_paa, start_paa_url,
-                                paa_list, initialSet, query, browser, depth)
-        # print(paa_list)
-        query_questions = []
-        flatten_paa_list(paa_list, query_questions, query)
-        result[query] = query_questions.copy()
+                    crawlQuestions_Mingyang(start_paa, start_paa_url,
+                                            paa_list, initialSet, query, browser, depth)
+                    # print(paa_list)
+                    query_questions = []
+                    flatten_paa_list(paa_list, query_questions, query)
+
+                    result[query] += query_questions.copy()
+
+                    # Save the result to a json file, make sure the old ones are good
+                    with open(args.out_dir, 'w') as fp:
+                        json.dump(result, fp, sort_keys=False, indent=4)
+                except:
+                    print("[FAILED_PPA] Unable to extract the questions for: {}".format(query))
+    
+    elif args.extraction_mode == "full":
+        from gquestions_json import newSearch_Mingyang, crawlQuestions_Mingyang, flatten_paa_list
+        for query in queries:
+            if not result.get(query, None):
+                #try:
+                result[query] = []
+
+                start_paa, start_paa_url, start_paa_answer = newSearch_Mingyang(
+                    browser, query)
+
+                # initialSet = {}
+                # cnt = 0
+                # for q, url, a in zip(start_paa, start_paa_url, start_paa_answer):
+                #     initialSet.update({cnt: {"q": q, "url": url, "a": a}})
+                #     cnt += 1
+
+                # paa_list = []
+
+                # crawlQuestions_Mingyang(start_paa, start_paa_url, start_paa_answer,
+                #                         paa_list, initialSet, query, browser, depth)
+                # # print(paa_list)
+                # query_questions = []
+                # flatten_paa_list(paa_list, query_questions, query)
+
+                # result[query] += query_questions.copy()
+
+                # # Save the result to a json file, make sure the old ones are good
+                # with open(args.out_dir, 'w') as fp:
+                #     json.dump(result, fp, sort_keys=False, indent=4)
+                # except:
+                #     print("[FAILED_PPA] Unable to extract the questions for: {}".format(query))
+
+
+
         # print(result)
 
-    # Save the result to a json file
-    with open(args.out_dir, 'w') as fp:
-        json.dump(result, fp, sort_keys=False, indent=4)
 
     browser.close()
